de211ce69e61eafc66b86c1908e2a403b6a425cb
Thomas Schreiner <iteano.thom@hotmail.com>
2022-09-27 20:15:38 -0300

    feat: add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect } from "react"
 import { useState } from "react"
 import "./App.css"
@@ -5,17 +6,35 @@ import { CharList } from "./components/Char/CharList"
 
 export function App() {
     const [characters, setCharacters] = useState([])
+    const [currentPage, setCurrentPage] = useState("https://rickandmortyapi.com/api/character/")
+    const [page, setPage] = useState({})
 
     useEffect(() => {
-        fetch("https://rickandmortyapi.com/api/character/")
+        fetch(currentPage)
             .then((resp) => resp.json())
-            .then((resp) => setCharacters(resp.results))
+            .then((resp) => {
+                setCharacters(resp.results)
+                setPage({ next: resp.info.next, prev: resp.info.prev })
+            })
             .catch((erro) => console.log(erro))
-    }, [])
+    }, [currentPage])
+
+    function pageNumber() {
+        return page.next && page.next.split("?page=")[1] - 1
+    }
 
     return (
         <div className="App">
             <CharList characters={characters} />
+            <div className="pagination">
+                <a href={page.prev && "#"}>
+                    <button onClick={() => page.prev && setCurrentPage(page.prev)}>Anterior</button>
+                </a>
+                <span>{pageNumber()}</span>
+                <a href={page.next && "#"}>
+                    <button onClick={() => page.next && setCurrentPage(page.next)}>Próxima</button>
+                </a>
+            </div>
         </div>
     )
 }
